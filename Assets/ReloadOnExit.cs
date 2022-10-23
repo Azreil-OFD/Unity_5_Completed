bbb0043f072ee14fc662d51c8bb912af2ee0e7ae
Azreil-OFD <fantom2413@gmail.com>
2022-10-23 13:51:08 +0300

    init

---
+++ Assets/ReloadOnExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadOnExit : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
